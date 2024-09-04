c1349c2481498349a309ee6d4a0f345092973e8e
ripter <ripter001@gmail.com>
2024-09-04 11:17:57 -0400

    Can run basic window raylib example in Forth

--- csrc/raylib/pf_raylib.h
+++ csrc/raylib/pf_raylib.h
@@ -3,14 +3,28 @@
 
 /* Define the XT values for the raylib words. */
 #define RAYLIB_XT_VALUES \
-    ID_INIT_WINDOW         
+    ID_INIT_WINDOW, \
+    ID_SET_TARGET_FPS, \
+    ID_WINDOW_SHOULD_CLOSE, \
+    ID_BEGIN_DRAWING, \
+    ID_CLEAR_BACKGROUND, \
+    ID_DRAW_TEXT, \
+    ID_END_DRAWING, \
+    ID_CLOSE_WINDOW \
 
 
 
 /* Macro to add all the XT tokens to the dictionary */
 #define ADD_RAYLIB_WORDS_TO_DICTIONARY \
     pfDebugMessage("pfBuildDictionary: Adding raylib words\n"); \
-    CreateDicEntryC( ID_INIT_WINDOW, "INIT-WINDOW", 0 ); 
+    CreateDicEntryC( ID_INIT_WINDOW, "INIT-WINDOW", 0 ); \
+    CreateDicEntryC( ID_SET_TARGET_FPS, "SET-TARGET-FPS", 0 ); \
+    CreateDicEntryC( ID_WINDOW_SHOULD_CLOSE, "WINDOW-SHOULD-CLOSE", 0 ); \
+    CreateDicEntryC( ID_BEGIN_DRAWING, "BEGIN-DRAWING", 0 ); \
+    CreateDicEntryC( ID_CLEAR_BACKGROUND, "CLEAR-BACKGROUND", 0 ); \
+    CreateDicEntryC( ID_DRAW_TEXT, "DRAW-TEXT", 0 ); \
+    CreateDicEntryC( ID_END_DRAWING, "END-DRAWING", 0 ); \
+    CreateDicEntryC( ID_CLOSE_WINDOW, "CLOSE-WINDOW", 0 ); \
 
 
 /* Define the raylib words */
@@ -20,31 +34,73 @@
         CharPtr = (char *) M_POP;  /* title string, not null terminated. */ \
         cell_t height = M_POP; \
         cell_t width = M_POP; \
-        if (Temp != NULL && len > 0 && len < TIB_SIZE) { \
+        if (CharPtr != NULL && len > 0 && len < TIB_SIZE) { \
             M_DROP; \
             pfCopyMemory(gScratch, CharPtr, len); \
             gScratch[len] = '\0'; \
             InitWindow(width, height, gScratch); \
-            printf( "\nInitWindow( %d, %d, %d, %s )", width, height, len, (char *)gScratch ); \
+            printf( "\nInitWindow( %d, %d, %d, %s )\n", width, height, len, (char *)gScratch ); \
         } else { \
-            fprintf(stderr, "Error: Invalid string or length. Please use s\" to create a simple string.\n"); \
+            fprintf(stderr, "\nError: Invalid string or length. Please use s\" to create a simple string.\n"); \
             break; \
         } \
         break; \
-    }
+    } \
+    case ID_SET_TARGET_FPS: {  /* ( +n --  ) */ \
+        SetTargetFPS(TOS); \
+        M_DROP; \
+        break; \
+    } \
+    case ID_WINDOW_SHOULD_CLOSE: {  /* ( -- +n ) */ \
+        int result = WindowShouldClose(); \
+        printf( "\nWindowShouldClose() = %d \n", result ); \
+        M_PUSH(result); \
+        break; \
+    } \
+    case ID_BEGIN_DRAWING: {  /* ( --  ) */ \
+        BeginDrawing(); \
+        break; \
+    } \
+    case ID_CLEAR_BACKGROUND: {  /* ( n n n n --  ) */ \
+        int red = M_POP; \
+        int green = M_POP; \
+        int blue = M_POP; \
+        int alpha = TOS; \
+        M_DROP; \
+        ClearBackground((Color){ red, blue, green, alpha }); \
+        break; \
+    } \
+    case ID_DRAW_TEXT: {  /* ( +n +n c-addr u n n n n --  ) */ \
+        int alpha = TOS; \
+        int blue = M_POP; \
+        int green = M_POP; \
+        int red = M_POP; \
+        int fontSize = M_POP; \
+        int posY = M_POP; \
+        int posX = M_POP; \
+        int len = M_POP; \
+        CharPtr = (char *) M_POP;  /* title string, not null terminated. */ \
+        if (CharPtr != NULL && len > 0 && len < TIB_SIZE) { \
+            M_DROP; \
+            pfCopyMemory(gScratch, CharPtr, len); \
+            gScratch[len] = '\0'; \
+            DrawText(gScratch, posX, posY, fontSize, (Color){ red, green, blue, alpha }); \
+        } \
+        break; \
+    } \
+    case ID_END_DRAWING: {  /* ( --  ) */ \
+        EndDrawing(); \
+        break; \
+    } \
+    case ID_CLOSE_WINDOW: {  /* ( --  ) */ \
+        CloseWindow(); \
+        break; \
+    } \
 
 #endif  /* _raylib_pf_raylib_h */
 
-//         cell_t width = M_POP; 
-//         InitWindow( 800, 600, "raylib-pforth" ); 
-//         // char* title = (char*) TOS; 
+            // void DrawText(const char *text, int posX, int posY, int fontSize, Color color); 
+            // DrawText(gScratch, x, y, fontSize, BLACK); 
+            // printf( "\nDrawText( %s, %d, %d, %d, (Color){ %d %d %d %d } )", (char *)gScratch, posX, posY, fontSize, red, green, blue, alpha ); 
 
-//         pfCopyMemory( gScratch, (char *) Temp, (ucell_t) TOS ); 
-//         gScratch[TOS] = '\0'; 
 
-        // if (len >= TIB_SIZE) { 
-        //     printf("String length exceeds buffer size\n"); 
-        //     break; 
-        // } \
-        // pfCopyMemory( gScratch, (char *) title, len ); 
-        // gScratch[len] = '\0'; 
---
+++ examples/wip.fth
@@ -0,0 +1,3 @@
+s" Congrats! You opened a window from Forth!" 190 200 20 ORANGE 
+.s
+draw-text
